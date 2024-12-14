667559c1fd83ab7c86d4548b341331a776eaf6cc
yassers25 <152821478+yassers25@users.noreply.github.com>
2024-12-14 17:52:41 +0100

    Update DatabaseServices.java
    add where column is not null

--- src/main/java/com/db/DatabaseServices.java
+++ src/main/java/com/db/DatabaseServices.java
@@ -75,37 +75,37 @@ public class DatabaseServices {
 
     // Récupérer le nombre d'offres par ville
     public static Map<String, Integer> getJobsByCity() {
-        String query = "SELECT city, COUNT(*) as total_jobs FROM jobs GROUP BY city ORDER BY total_jobs DESC";
+        String query = "SELECT city, COUNT(*) as total_jobs FROM jobs WHERE city IS NOT NULL GROUP BY city ORDER BY total_jobs DESC";
         return executeQuery(query);
     }
 
     // Récupérer le nombre d'offres par secteur d'activité
     public static Map<String, Integer> getJobsByActivitySector() {
-        String query = "SELECT activity_sector, COUNT(*) AS total_jobs FROM jobs GROUP BY activity_sector ORDER BY total_jobs DESC";
+        String query = "SELECT activity_sector, COUNT(*) AS total_jobs FROM jobs WHERE activity_sector IS NOT NULL GROUP BY activity_sector ORDER BY total_jobs DESC";
         return executeQuery(query);
     }
 
     // Récupérer le nombre d'offres par expérience requise
     public static Map<String, Integer> getJobsByExperience() {
-        String query = "SELECT required_experience, COUNT(*) AS total_jobs FROM jobs GROUP BY required_experience ORDER BY total_jobs DESC";
+        String query = "SELECT required_experience, COUNT(*) AS total_jobs FROM jobs WHERE required_experience IS NOT NULL GROUP BY required_experience ORDER BY total_jobs DESC";
         return executeQuery(query);
     }
 
     // Récupérer le nombre d'offres par niveau d'études
     public static Map<String, Integer> getJobsByStudyLevel() {
-        String query = "SELECT study_level, COUNT(*) AS total_jobs FROM jobs GROUP BY study_level ORDER BY total_jobs DESC";
+        String query = "SELECT study_level, COUNT(*) AS total_jobs FROM jobs WHERE study_level IS NOT NULL GROUP BY study_level ORDER BY total_jobs DESC";
         return executeQuery(query);
     }
 
     // Récupérer le nombre d'offres par type de contrat
     public static Map<String, Integer> getJobsByContractType() {
-        String query = "SELECT contract_type, COUNT(*) AS total_jobs FROM jobs GROUP BY contract_type ORDER BY total_jobs DESC";
+        String query = "SELECT contract_type, COUNT(*) AS total_jobs FROM jobs WHERE contract_type IS NOT NULL GROUP BY contract_type ORDER BY total_jobs DESC";
         return executeQuery(query);
     }
 
     // Récupérer le nombre d'offres par télétravail
     public static Map<String, Integer> getJobsByRemoteWork() {
-        String query = "SELECT remote_work, COUNT(*) AS total_jobs FROM jobs GROUP BY remote_work ORDER BY total_jobs DESC";
+        String query = "SELECT remote_work, COUNT(*) AS total_jobs FROM jobs WHERE remote_work IS NOT NULL GROUP BY remote_work ORDER BY total_jobs DESC";
         return executeQuery(query);
     }
 
